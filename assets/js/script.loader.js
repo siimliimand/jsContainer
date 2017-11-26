d4de765abb6b73ffe525c69a39a560f45ceee07e
Siim Liimand <siim.liimand@gmail.com>
2017-11-26 17:28:08 +0200

    v2

--- assets/js/script.loader.js
+++ assets/js/script.loader.js
@@ -1,7 +1,7 @@
 function ScriptLoader() {
     this.configs = {
         css: {
-            location: document.head,
+            location: document.body,
             attributes: {
                 rel: 'stylesheet',
                 href: '#',
@@ -9,6 +9,16 @@ function ScriptLoader() {
             },
             urlAttribute: 'href',
             tagName: 'link'
+        },
+        css2: {
+            location: document.body,
+            attributes: {
+                rel: 'stylesheet',
+                href: '#',
+                type: 'text/css'
+            },
+            urlAttribute: false,
+            tagName: 'style'
         },
         js: {
             location: document.body,
@@ -35,7 +45,9 @@ ScriptLoader.prototype.addScript = function (type, url, onload) {
     var location = config.location;
     var attributes = config.attributes;
     var urlAttribute = config.urlAttribute;
-    attributes[urlAttribute] = url;
+    if(urlAttribute) {
+        attributes[urlAttribute] = url;
+    }
     attributes.onload = function() {
         self.callback(onload);
         if(type === 'js') {
@@ -48,7 +60,23 @@ ScriptLoader.prototype.addScript = function (type, url, onload) {
     for (var key in attributes) {
         element[key] = attributes[key];
     }
-    location.appendChild(element);
+    
+    if(type === 'css2') {
+        this.load(url, function(responseText) {
+            element.innerHTML = responseText;
+            location.appendChild(element);
+        });
+    } else {
+        location.appendChild(element);
+    }
+};
+ScriptLoader.prototype.load = function(file, done) {
+    var client = new XMLHttpRequest();
+    client.open('GET', file);
+    client.onreadystatechange = function() {
+        done(this.responseText);
+    };
+    client.send();
 };
 ScriptLoader.prototype.callback = function(callback) {
     if(typeof callback === 'function') {
